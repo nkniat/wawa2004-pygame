8a8a3fbe6ec6355f1b6e26c671682b6a7024f8fd
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-04-20 10:47:07 +0200

    podstawy cd.

--- cw1_podstawy.py
+++ cw1_podstawy.py
@@ -18,15 +18,36 @@ while True:
             exit()
 
     #pygame.draw.rect(screen, "White", (X, Y, FIGURE_WIDTH, FIGURE_HEIGHT))
-    pygame.draw.line(screen, (255, 0, 0), (X, Y), (X, Y + 200), 5)  # linia pionowa
-    pygame.draw.line(screen, (0, 255, 0), (X, Y), (X + 200, Y), 5)  # linia pozioma
-    # pygame.draw.line(screen, (0, 0, 255), (X, Y), (X + 200, Y + 200), 5)  # linia po przekątnej
+    # pygame.draw.line(screen, (255, 0, 0), (X, Y), (X, Y + 200), 5)  # linia pionowa
+    # pygame.draw.line(screen, (0, 255, 0), (X, Y), (X + 200, Y), 5)  # linia pozioma
+    # # pygame.draw.line(screen, (0, 0, 255), (X, Y), (X + 200, Y + 200), 5)  # linia po przekątnej
 
-    step = 0
-    for _ in range(WIDTH):
-        for _ in range(HEIGHT):
-            pygame.draw.rect(screen, "White", (X + step, Y + step, FIGURE_WIDTH, FIGURE_HEIGHT))
-            step += 10
+    # step = 0
+    # for _ in range(WIDTH):
+    #     for _ in range(HEIGHT):
+    #         pygame.draw.rect(screen, "White", (X + step, Y + step, FIGURE_WIDTH, FIGURE_HEIGHT))
+    #         step += 10
+
+    # # wypisywanie tekstu
+    # font = pygame.font.SysFont('Arial', 24)
+    # label = font.render('Hello World!', 1, 'White')
+    # # żeby pokazać napis na ekranie
+    # screen.blit(label, (X, Y))
+
+    step = 5
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        X -= step
+    if keys[pygame.K_RIGHT]:
+        X += step
+    if keys[pygame.K_UP]:
+        Y -= step
+    if keys[pygame.K_DOWN]:
+        Y += step
+
+    screen.fill('Black')
+    pygame.draw.rect(screen, "White", (X, Y, FIGURE_WIDTH, FIGURE_HEIGHT))
 
     pygame.display.update()
     clock.tick(60)
