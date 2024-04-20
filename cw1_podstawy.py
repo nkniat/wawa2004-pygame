82e39b9a67633b0bff8c6eb1fc9fe583d90c38da
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-04-20 10:09:56 +0200

    szkielet

---
+++ cw1_podstawy.py
@@ -0,0 +1,32 @@
+import pygame
+from sys import exit
+pygame.init()  # zainicjalizowanie silnika pygame
+
+WIDTH, HEIGHT = 800, 400
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Moja pierwsza gra')
+clock = pygame.time.Clock()
+
+X, Y = 10, 10
+FIGURE_WIDTH, FIGURE_HEIGHT = 10 , 10
+
+# nieskonczona pętla do utrzymania okienka gry
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    #pygame.draw.rect(screen, "White", (X, Y, FIGURE_WIDTH, FIGURE_HEIGHT))
+    pygame.draw.line(screen, (255, 0, 0), (X, Y), (X, Y + 200), 5)  # linia pionowa
+    pygame.draw.line(screen, (0, 255, 0), (X, Y), (X + 200, Y), 5)  # linia pozioma
+    # pygame.draw.line(screen, (0, 0, 255), (X, Y), (X + 200, Y + 200), 5)  # linia po przekątnej
+
+    step = 0
+    for _ in range(WIDTH):
+        for _ in range(HEIGHT):
+            pygame.draw.rect(screen, "White", (X + step, Y + step, FIGURE_WIDTH, FIGURE_HEIGHT))
+            step += 10
+
+    pygame.display.update()
+    clock.tick(60)
